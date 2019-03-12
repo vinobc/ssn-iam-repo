e888ddb1271c895235a6eabaa4c0eb8f5c94da69
R.Vinob chander <abvp3121@gmail.com>
2019-03-12 16:02:26 +0530

    added test.html

--- routes.py
+++ routes.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
-# from models import db
+from models import db
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/ssnim'
-# db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/ssnim'
+db.init_app(app)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route("/about")
-def about():
-	return render_template("about.html")
+@app.route("/test")
+def test():
+	return render_template("test.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
